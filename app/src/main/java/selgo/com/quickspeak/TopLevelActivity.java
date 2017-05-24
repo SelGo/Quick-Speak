bc879121ba20175956e90e82f26dbdcf75b863ec
SelGo <tsampakiourispetros@gmail.com>
2017-05-24 11:16:47 +0300

    Made changes to the TopLevelActivity layout. Added a new activity which will include the word categories for the English language.

--- app/src/main/java/selgo/com/quickspeak/TopLevelActivity.java
+++ app/src/main/java/selgo/com/quickspeak/TopLevelActivity.java
@@ -1,7 +1,10 @@
 package selgo.com.quickspeak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        TextView english = (TextView) findViewById(R.id.english_name);
+        english.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent englishIntent = new Intent(TopLevelActivity.this, EnglishWordsCategories.class);
+                startActivity(englishIntent);
+            }
+        });
     }
 }
