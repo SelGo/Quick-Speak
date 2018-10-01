08a8b19435057aa5d06666282c83647889751484
SelGo <tsampakiourispetros@gmail.com>
2018-10-01 11:20:37 +0300

    Finished database development. The app no longer uses the EnglishWord and WordAdapter classes, thus they are deleted from the project. Real data can be inserted to the database from now on.

---
+++ app/src/main/java/selgo/com/quickspeak/WordCursorAdapter.java
@@ -0,0 +1,33 @@
+package selgo.com.quickspeak;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+public class WordCursorAdapter extends CursorAdapter {
+
+    public WordCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView word = (TextView) view.findViewById(R.id.language_word);
+        TextView translatedWord = (TextView) view.findViewById(R.id.translated_word);
+
+        String wordText = cursor.getString(1);
+        String translatedWordText = cursor.getString(2);
+
+        word.setText(wordText);
+        translatedWord.setText(translatedWordText);
+    }
+}
--- app/src/main/java/selgo/com/quickspeak/WordListContainer.java
+++ app/src/main/java/selgo/com/quickspeak/WordListContainer.java
@@ -2,27 +2,27 @@ package selgo.com.quickspeak;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import selgo.com.quickspeak.data.WordContract;
 import selgo.com.quickspeak.data.WordContract.WordEntry;
 import selgo.com.quickspeak.data.WordDbHelper;
 
 public class WordListContainer extends Activity {
 
     private String[] titles = EnglishLevel.getWordsCategories();
-    private ArrayList<EnglishWord> words = new ArrayList<>();
+
     private static String[] arrayDefault = {""};
     private ArrayAdapter<String> adapterDefault;
-    private WordAdapter wordAdapter;
     private ListView listView;
+    private SQLiteDatabase db;
+    private Cursor cursor;
+    private WordDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,32 +32,31 @@ public class WordListContainer extends Activity {
 
         switch (position) {
             case 0:
-                displayDatabaseInfo();
                 getActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorEnglish)));
                 getActionBar().setTitle(titles[0]);
 
-                words.add(new EnglishWord("A", "a"));
-                words.add(new EnglishWord("B", "b"));
-                words.add(new EnglishWord("C", "c"));
-                words.add(new EnglishWord("D", "d"));
+                try {
+                    mDbHelper = new WordDbHelper(this);
+                    db = mDbHelper.getReadableDatabase();
 
-                wordAdapter = new WordAdapter(this, words);
-                listView = (ListView) findViewById(R.id.word_items_list);
-                listView.setAdapter(wordAdapter);
+                    cursor = db.query(WordEntry.TABLE_NAME,
+                            new String[]{WordEntry._ID, WordEntry.COLUMN_ENGLISH_WORD, WordEntry.COLUMN_TRANSLATED_WORD},
+                            WordEntry.COLUMN_LEVEL + "=?",
+                            new String[] {"Level 1"},
+                            null, null, null);
+
+                    WordCursorAdapter wordCursorAdapter = new WordCursorAdapter(this, cursor);
+
+                    listView = (ListView) findViewById(R.id.word_items_list);
+                    listView.setAdapter(wordCursorAdapter);
+                } catch (SQLException e) {
+                    Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
                 break;
             case 1:
-                displayDatabaseInfo();
                 getActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorRed)));
                 getActionBar().setTitle(titles[1]);
-
-                words.add(new EnglishWord("English Word", "Translated Word"));
-                words.add(new EnglishWord("English Word", "Translated Word"));
-                words.add(new EnglishWord("English Word", "Translated Word"));
-
-                wordAdapter = new WordAdapter(this, words);
-                listView = (ListView) findViewById(R.id.word_items_list);
-                listView.setAdapter(wordAdapter);
-                break;
             default:
                 getActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorDefault)));
                 getActionBar().setTitle(R.string.app_name);
@@ -69,31 +68,10 @@ public class WordListContainer extends Activity {
         }
     }
 
-    /**
-     *  Temporary method to test if quick_speak.db database is created properly and it's ready for using.
-     */
-    private void displayDatabaseInfo() {
-        WordDbHelper mDbHelper = new WordDbHelper(this);
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        Cursor cursor = db.query(WordEntry.TABLE_NAME,
-                new String[]{WordEntry.COLUMN_ENGLISH_WORD, WordEntry.COLUMN_TRANSLATED_WORD},
-                WordEntry.COLUMN_LEVEL + "=?",
-                new String[] {"Level 1"},
-                null, null, null);
-
-        /*Cursor cursor = db.rawQuery("SELECT * FROM " + WordEntry.TABLE_NAME, null);*/
-
-
-        try {
-            TextView dbTestText = (TextView) findViewById(R.id.db_test_text);
-            dbTestText.setText(cursor.getCount() + "");
-            /*if(cursor.moveToFirst()) {
-                dbTestText.setText(cursor.getString(0));
-            }*/
-        } finally {
-            cursor.close();
-            db.close();
-        }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
